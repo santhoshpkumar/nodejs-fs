01536f9f140b7734d5fe83dcf7e10055a32bc6de
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-09-13 22:42:39 +0530

    Added comments

--- js/callbacksconcepts.js
+++ js/callbacksconcepts.js
@@ -6,6 +6,8 @@ var fs = require("fs");
 
 console.log("***************");
 console.log("Program Started");
+
+// Read the file asynchronously, the read output is returned via a callback
 fs.readFile('input.txt', function (err, data) {
     if (err) return console.error(err);
     console.log("Async read output:");
@@ -14,12 +16,12 @@ fs.readFile('input.txt', function (err, data) {
     console.log("*********************");
 });
 
+// Read the file synchronously, this will block the event loop until file read is complete.
 var data = fs.readFileSync('input.txt');
 
 console.log("Sync read output:");
 console.log(data.toString());
 console.log("Completed Sync read.");
-
 console.log("Program Ended");
 console.log("*************");
 // Note that the in the output the async read might happen after the end of execution of all program statements.
