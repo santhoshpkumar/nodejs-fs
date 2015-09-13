f24c7104dfb9c6866ca2fe8b3ed1e50a520e0616
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-09-13 22:46:37 +0530

    Added comments

--- js/callbacksconcepts.js
+++ js/callbacksconcepts.js
@@ -25,3 +25,14 @@ console.log("Completed Sync read.");
 console.log("Program Ended");
 console.log("*************");
 // Note that the in the output the async read might happen after the end of execution of all program statements.
+
+// These two examples explain the concept of blocking and non-blocking calls.
+// Second example shows that program blocks until it reads the file and then only
+// it proceeds to end the program where as in First example, program does not wait
+// for file reading but it just proceeded to print "Program Ended" and same time
+// program without blocking continues reading the file.
+
+// Thus, a blocking program executes very much in sequence and from programming point
+// of view its easier to implement the logic but non-blocking programs does not execute
+// in sequence, so in case a program needs to use any data to be processed, it should
+// be kept with-in the same block to make it sequential execution.
